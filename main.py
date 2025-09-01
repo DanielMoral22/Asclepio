532a81b1e0181f2e254780503837b4fa2b454caa
DanielMoral22 <danilsmorales8@gmail.com>
2025-08-31 20:57:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def recibir():
+    datos = request.get_json()
+    print("Datos recibidos:", datos)
+    return jsonify({"status": "ok", "mensaje": "Datos recibidos"}), 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # usa el puerto 5000 por defecto o el que te dé Railway
+    app.run(host='0.0.0.0', port=port)
 
