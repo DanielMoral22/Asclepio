9f5d796471646ebb93a2278972c4b0b49e59b51c
Dr. Daniel Moral Planck <danilsmorales8@gmail.com>
2026-09-23 23:49:09 +0000

    2ª rama

--- experimentos.py
+++ experimentos.py
@@ -3,3 +3,6 @@ print("putos judios de mierda")
 
 print("Los judios chinguen a su madre jajajaja")
 print(8*9)
+
+#Amo esta biblioteca y encoentre algo que en verdad me gusta
+print ("ya me esta empezando a dar hambre jaja")
